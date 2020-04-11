089aa3a8e2d57c4df4ac1f1f2726651fe2a5ca10
GOGOROBO <andrewjheckel@gmail.com>
2020-04-11 16:56:33 -0400

    Basic changes

--- App.js
+++ App.js
@@ -3,15 +3,17 @@ import { Text, View, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { MaterialCommunityIcons } from 'react-native-vector-icons';
-function Feed() {
+import {Calendar} from 'react-native-calendars';
+
+function Review() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Feed!</Text>
+      <Text>Review!</Text>
     </View>
   );
 }
 
-function Profile() {
+function Paperdoll() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
       
@@ -21,10 +23,47 @@ function Profile() {
   );
 }
 
-function Notifications() {
+function Settings() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Notifications!</Text>
+      <Calendar
+      // Initially visible month. Default = Date()
+      // current={'2012-03-01'}
+      // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
+      minDate={'2010-01-01'}
+      // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
+      //maxDate={'2012-05-30'}
+      // Handler which gets executed on day press. Default = undefined
+      onDayPress={(day) => {console.log('selected day', day)}}
+      // Handler which gets executed on day long press. Default = undefined
+      onDayLongPress={(day) => {console.log('selected day', day)}}
+      // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+      monthFormat={'yyyy MM'}
+      // Handler which gets executed when visible month changes in calendar. Default = undefined
+      onMonthChange={(month) => {console.log('month changed', month)}}
+      // Hide month navigation arrows. Default = false
+      hideArrows={true}
+      // Replace default arrows with custom ones (direction can be 'left' or 'right')
+      renderArrow={(direction) => (<Arrow/>)}
+      // Do not show days of other months in month page. Default = false
+      hideExtraDays={true}
+      // If hideArrows=false and hideExtraDays=false do not switch month when tapping on greyed out
+      // day from another month that is visible in calendar page. Default = false
+      disableMonthChange={true}
+      // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
+      firstDay={1}
+      // Hide day names. Default = false
+      hideDayNames={true}
+      // Show week numbers to the left. Default = false
+      showWeekNumbers={true}
+      // Handler which gets executed when press arrow icon left. It receive a callback can go back month
+      onPressArrowLeft={substractMonth => substractMonth()}
+      // Handler which gets executed when press arrow icon right. It receive a callback can go next month
+      onPressArrowRight={addMonth => addMonth()}
+      // Disable left arrow. Default = false
+      disableArrowLeft={true}
+      // Disable right arrow. Default = false
+      disableArrowRight={true}/>
     </View>
   );
 }
@@ -34,41 +73,42 @@ const Tab = createBottomTabNavigator();
 function MyTabs() {
   return (
     <Tab.Navigator
-      initialRouteName="Feed"
+      initialRouteName="Review"
       tabBarOptions={{
         activeTintColor: '#e91e63',
       }}
     >
       <Tab.Screen
-        name="Feed"
-        component={Feed}
+    name="Paperdoll"
+    component={Paperdoll}
+    options={{
+      tabBarLabel: 'Paperdoll',
+      tabBarIcon: ({ color, size }) => (
+        <MaterialCommunityIcons name="account" color={color} size={size} />
+      ),
+    }}
+  />
+      <Tab.Screen
+        name="Review"
+        component={Review}
         options={{
-          tabBarLabel: 'Home',
+          tabBarLabel: 'Review',
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
+            <MaterialCommunityIcons name="chart-bell-curve" color={color} size={size} />
           ),
         }}
       />
       <Tab.Screen
-        name="Notifications"
-        component={Notifications}
+        name="Settings"
+        component={Settings}
         options={{
-          tabBarLabel: 'Updates',
+          tabBarLabel: 'Settings',
           tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={size} />
+            <MaterialCommunityIcons name="settings" color={color} size={size} />
           ),
         }}
       />
-      <Tab.Screen
-        name="Profile"
-        component={Profile}
-        options={{
-          tabBarLabel: 'Profile',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
+      
     </Tab.Navigator>
   );
 }
